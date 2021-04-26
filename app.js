a72583efdaab6a8b4b92e72342898ac4bd8ceacd
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-04-26 16:07:11 +0500

    question 8 added

---
+++ app.js
@@ -0,0 +1,13 @@
+// **********CHAPTER 14-16**********
+// **********ARRAYS**********
+
+
+// *****QUESTION 8*****
+
+// var stdName = ["Michael", "John", "Tony"];
+// var stdScore = ["320", "230", "480"];
+// var stdPercentage = [(stdScore[0] / 500 * 100), (stdScore[1] / 500 * 100), (stdScore[2] / 500 * 100)];
+
+// document.write("<h3>" + "Score of " + stdName[0] + " is " + stdScore[0] + ". " + "Percentage: " + stdPercentage[0] + "%" + "<h3/>");
+// document.write("<h3>" + "Score of " + stdName[1] + " is " + stdScore[1] + ". " + "Percentage: " + stdPercentage[1] + "%" + "<h3/>");
+// document.write("<h3>" + "Score of " + stdName[2] + " is " + stdScore[2] + ". " + "Percentage: " + stdPercentage[2] + "%" + "<h3/>");
